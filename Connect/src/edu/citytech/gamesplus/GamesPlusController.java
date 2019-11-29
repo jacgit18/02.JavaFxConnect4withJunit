7336ddb0a34b8607bac446edc5b044bb50e37087
jac <joshuacarpentier21@gmail.com>
2019-11-29 02:44:49 -0500

    Add

--- Connect/src/edu/citytech/gamesplus/GamesPlusController.java
+++ Connect/src/edu/citytech/gamesplus/GamesPlusController.java
@@ -3,22 +3,30 @@ package edu.citytech.gamesplus;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import edu.citytech.service.Connect4Service;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 public class GamesPlusController implements Initializable {
 
     @FXML
     private GridPane GPane;
         
+    @FXML
+    private AnchorPane MainPane;
+    
     @FXML
     private Button btn_Reset;
     
@@ -53,12 +61,18 @@ public class GamesPlusController implements Initializable {
     
 	private void ClickedEvent(MouseEvent e) {
 		Label label = (Label)e.getSource();
-		if(! label.getText().equals("?")){
-			lbl_Message.setText("Invalid move as of " + new Date());
+//		if(! label.getText().equals("?")){
+//			lbl_Message.setText("Invalid move as of " + new Date());
+//		
+//		return;
+//	}
 		
-		return;
-	}
-	
+		if (label.getTextFill() != Color.WHITE) {
+			lbl_Message.setText("Not your turn");
+			return;
+		}
+		
+		
 	String xOrO = "";
 	if (isX) {
 		xOrO = "X";
@@ -74,6 +88,8 @@ public class GamesPlusController implements Initializable {
 		moves[i] = labels[i].getText();
 	}
 	
+	
+	
 	int cellNumber = (int) label.getUserData();
 	String newMove = label.getText();
 	moves[cellNumber] = "?";
@@ -85,6 +101,17 @@ public class GamesPlusController implements Initializable {
 	labels[nextMove].getStyleClass().add(xOrO);
 	
 	lbl_Message.setText("Current is " + cellNumber + " next valid move is " + nextMove + " clicked on " + new Date());
+
+
+//	if (moves.length != 0) {
+//		lbl_Message.setText("You are the winner!");
+//		GPane.getChildren().forEach(x -> {
+//			Label lbl = (Label) x;
+//			lbl.setDisable(true);
+//		});
+//	}
+	
+	
 		
 	}
 	
