10907259b0d955ac562981f9194d3932c3cb599e
jac <joshuacarpentier21@gmail.com>
2019-12-08 08:06:18 -0500

    add

--- Connect/src/edu/citytech/gamesplus/GamesPlusController.java
+++ Connect/src/edu/citytech/gamesplus/GamesPlusController.java
@@ -17,125 +17,108 @@ import javafx.scene.paint.Color;
 
 public class GamesPlusController implements Initializable {
 
-    @FXML
-    private GridPane GPane;
-        
-    @FXML
-    private AnchorPane MainPane;
-    
-    @FXML
-    private Button btn_Reset;
-    
-    
-    @FXML
-    private Label lbl_Message; 
+	@FXML
+	private GridPane GPane;
 
+	@FXML
+	private AnchorPane MainPane;
 
-    
-    @Override
+	@FXML
+	private Button btn_Reset;
+
+	@FXML
+	private Label lbl_Message;
+
+	private Label[] labels = new Label[42];
+
+	private boolean isX = false;
+
+	int win = 1;
+
+	@Override
 	public void initialize(URL url, ResourceBundle resource) {
-    	
-		
-		int i = 0;
-		for (int rows = 0; rows <= 5; rows++) 
-			for (int column = 0; column <= 6; column++) {
-			Label label = new Label("?");
-			labels[i] = label;
-			label.getStyleClass().add("custom-label");
-			label.setUserData(i);
-			label.setOnMouseClicked((MouseEvent e) -> this.ClickedEvent(e));
-			GPane.add(label, column, rows);
-			i++;
-		}
-	}
-    
-    private boolean isX = true;
-    
-	private void ClickedEvent(MouseEvent e) {
-		Label label = (Label)e.getSource();
 
+		int i = 0;
+		for (int rows = 0; rows <= 5; rows++)
+			for (int column = 0; column <= 6; column++) {
+				Label label = new Label("?");
+				labels[i] = label;
+				label.getStyleClass().add("custom-label");
+				label.setUserData(i);
+				label.setOnMouseClicked((MouseEvent e) -> this.ClickedEvent(e));
+				GPane.add(label, column, rows);
+				i++;
+			}
+	}
+
+	private void ClickedEvent(MouseEvent e) {
+		Label label = (Label) e.getSource();
+		String[] moves = new String[42];
+
+		for (int i = 0; i < labels.length; i++) {
+			moves[i] = labels[i].getText();
+
+		}
+
+		int[] winner = Connect4Service.getWinner(moves);
 		
+		if (win == 1 && winner.length < 4) {
+
+
 		if (label.getTextFill() != Color.WHITE) {
 			lbl_Message.setText("Not your turn");
 			return;
 		}
-		
-		
-	String xOrO = "";
-	if (isX) {
-		xOrO = "X";
+
+		String xOrO = "";
+		if (isX) {
+			xOrO = "X";
+
+		} else {
+			xOrO = "O";
+
+		}
+		isX = !isX;
+		label.setText(xOrO);
+
+		int columnNumber = (int) label.getUserData();
+		String newMove = label.getText();
+		moves[columnNumber] = "?";
+		label.setText("?");
+		int nextMove = Connect4Service.getValidCellMoves(moves, columnNumber);
+
+		labels[nextMove].setText(newMove);
+		labels[nextMove].getStyleClass().add(xOrO);
+
 
 		
-//		 Label msg = new Label("GamesPlus.css");
-//	     msg.setStyle("-fx-background-color: blue; ");
-
+		if (winner.length == 4) {
+			lbl_Message.setText("Winner was found");
+			win = 0;
+		}
+		
 	} else {
-		xOrO = "O";
 
-	}
-	isX =!isX;
-	label.setText(xOrO);
-	
-	String[] moves = new String[42];
-	
-	for (int i = 0; i < labels.length; i++) {
-		moves[i] = labels[i].getText();
+		lbl_Message.setText("Game has already been won." );
 
-	}
-
-	
-	
-	int columnNumber = (int) label.getUserData();
-	String newMove = label.getText();
-	moves[columnNumber] = "?";
-	label.setText("?");
-	
-	int nextMove = Connect4Service.getValidCellMoves(moves, columnNumber);
-	
-	labels[nextMove].setText(newMove);
-	labels[nextMove].getStyleClass().add(xOrO);
-	
-    int [] winner = Connect4Service.getWinner(moves);
-	
-  
-    
-    
-	if (winner.equals(winner)) {
-		lbl_Message.setText("Winner was found");
-	}
-
-
-	
-	lbl_Message.setText("Current is " + columnNumber + " next valid move is " + nextMove + " clicked on " + new Date());
-
-
-	
-	
-//	if (winner = true) {
-//		lbl_Message.setText("You are the winner!");
-//	GPane.getChildren().forEach(x -> {
-//		Label lbl = (Label) x;
-//		lbl.setDisable(true);
-//		});
-//	}
-	
-	
 		
+//		lbl_Message.setText(
+//				"Current is " + columnNumber + " next valid move is " + nextMove + " clicked on " + new Date());
 	}
-	
-	private Label[] labels = new Label[42];
-	
+
+
+	}
+
 	@FXML
 	void reset(ActionEvent event) {
-		
+
 		for (Label label : labels) {
 			label.setText("?");
 			label.getStyleClass().removeIf(e -> e.equals("X") || e.equals("O"));
 		}
 		isX = true;
 		lbl_Message.setText("");
-		
+
 	}
-	
-	
-}   
+
+}
